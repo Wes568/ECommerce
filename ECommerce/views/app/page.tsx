a31b9f420c00725656a82969250b76836a39543d
Lucas Zamora <lucas@phac.com>
2024-11-21 11:08:26 -0300

    chore: install next and shadcn

---
+++ ECommerce/views/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export const Home = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <Button>shadcn</Button>
+    </div>
+  );
+};
+
+export default Home;
